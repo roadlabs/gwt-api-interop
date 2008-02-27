647db63714fb367b182fa2d806a41f453093d3c3
gwt.team.bobv <gwt.team.bobv@c4e34f52-af32-0410-b6ef-6f9209bb81f8>
2008-02-27 19:56:31 +0000

    Add a test case to explicitly call out JSIO behavior when returning undefined, null, and inappropriate types in the bean accessor case. Add a test suite for all JSIO tests. Add a test module for JSIO.
    Patch by: zundel, bobv

---
+++ trunk/user/test/com/google/gwt/jsio/JSIOTestSuite.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2008 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.gwt.jsio;
+
+import com.google.gwt.jsio.client.JSExporterTest;
+import com.google.gwt.jsio.client.JSFlyweightWrapperTest;
+import com.google.gwt.jsio.client.JSIOBeanReturnUndefined;
+import com.google.gwt.jsio.client.JSONInvokerTest;
+import com.google.gwt.jsio.client.JSONWrapperTest;
+import com.google.gwt.jsio.client.JsoOverrideTest;
+import com.google.gwt.jsio.rebind.NamePolicyTest;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * TestSuite for the JSIO API.
+ */
+public class JSIOTestSuite extends TestCase {
+  public static Test suite() {
+    TestSuite suite = new TestSuite("Test for the JSIO API");
+
+    suite.addTestSuite(JSExporterTest.class);
+    suite.addTestSuite(JSFlyweightWrapperTest.class);
+    suite.addTestSuite(JSIOBeanReturnUndefined.class);
+    suite.addTestSuite(JSONInvokerTest.class);
+    suite.addTestSuite(JSONWrapperTest.class);
+    suite.addTestSuite(JsoOverrideTest.class);
+    suite.addTestSuite(NamePolicyTest.class);
+
+    return suite;
+  }
+}
