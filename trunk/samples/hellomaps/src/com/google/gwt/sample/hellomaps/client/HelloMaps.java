d80b548eaca186de84b233fbf3fc6ad95edf2ac8
gwt.team.zundel <gwt.team.zundel@c4e34f52-af32-0410-b6ef-6f9209bb81f8>
2008-03-10 16:48:41 +0000

    Updates the HelloMaps sample to use annotations.
    Patch by: zundel
    Review by: bobv

--- trunk/samples/hellomaps/src/com/google/gwt/sample/hellomaps/client/HelloMaps.java
+++ trunk/samples/hellomaps/src/com/google/gwt/sample/hellomaps/client/HelloMaps.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006 Google Inc.
+ * Copyright 2008 Google Inc.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -19,6 +19,7 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.KeyboardListenerAdapter;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
@@ -45,20 +46,33 @@ public class HelloMaps implements EntryPoint {
     // Set the map up in a Dialog box, just for fun.
     final DialogBox dialog = new DialogBox(false, false);
     final Map theMap = new Map();
-    TextBox tb = new TextBox();
+    final Button findButton = new Button("Address:");
+    final TextBox tb = new TextBox();
     tb.addKeyboardListener(new KeyboardListenerAdapter() {
+      @Override
       public void onKeyPress(Widget sender, char keyCode, int modifiers) {
         if (keyCode == KEY_ENTER) {
-          theMap.setLocation(((TextBox)sender).getText());
+          theMap.setLocation(((TextBox) sender).getText());
         } else if (keyCode == KEY_ESCAPE) {
           dialog.removeFromParent();
         }
       }
     });
+    findButton.addClickListener(new ClickListener() {
+      public void onClick(Widget sender) {
+        theMap.setLocation(tb.getText());
+      }
+    });
     tb.setWidth("100%");
-    
-    VerticalPanel vp = new VerticalPanel();
-    vp.add(tb);
+
+    final HorizontalPanel hp = new HorizontalPanel();
+    hp.add(findButton);
+    hp.setCellWidth(findButton, "15%");
+    hp.add(tb);
+    hp.setCellWidth(tb, "85%");
+
+    final VerticalPanel vp = new VerticalPanel();
+    vp.add(hp);
     vp.add(theMap);
 
     dialog.setText("Drag me!");
